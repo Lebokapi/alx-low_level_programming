0fd24435d102bce26d32a2d312572eccee22361a
Lebokapi <themakapi@yahoo.com>
2023-03-18 10:20:55 -0700

    Alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main: Programme that prints  alphabet in lowercase
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char i;
+	for (i = 'a' ; i <= 'z' ; i++)
+		putchar(i);
+	putchar (\'n');
+	return (0);
+}
