d2deb14c2dcedbb39d6913614eb849dde6d12b34
Lebokapi <themakapi@yahoo.com>
2023-05-08 08:56:29 -0700

    CP

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -6,10 +6,10 @@ char *create_buffer(char *file);
 void close_file(int fd);
 
 /**
- * create_buffer - Allocates 1024 bytes for a buffer.
- * @file: The name of the file buffer is storing chars for.
+ * create_buffer - This allocates 1024 bytes for a buffer.
+ * @file: The name of the file buffer is storing chars for
  *
- * Return: A pointer to the newly-allocated buffer.
+ * Return: Pointer to the newly-allocated buffer.
  */
 char *create_buffer(char *file)
 {
